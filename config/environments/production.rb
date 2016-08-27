e9cca23f549a400c0976e23e5d1abbcc76f87dc6
vijay <vijay.ror@gmail.com>
2016-08-27 23:12:46 +0530

    added domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,6 +106,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+
   # sendgrid mail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
